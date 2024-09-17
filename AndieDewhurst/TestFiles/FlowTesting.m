74535902e748fdd2dfd781e159c0363c74dbc740
andiedewhurst <andie.dewhurst1208@gmail.com>
2024-09-17 12:44:39 -0500

    Notes from class

---
+++ AndieDewhurst/TestFiles/FlowTesting.m
@@ -0,0 +1,11 @@
+x = 69;
+
+if (x<4)
+    disp("Yep! Smaller than four! Teeny.")
+elseif ((x > 68) && (x < 70)) || ((x > 419) && (x < 421))
+    disp("nice.")
+elseif (x > 42)
+    disp("No, not smaller than four, but bigger than 42! Nice.")
+else 
+    disp("Psh. Lame number.")
+end
