a0853b5f60a81ddbcd49c0d9487af2d0282162e6
andiedewhurst <andie.dewhurst1208@gmail.com>
2024-09-19 14:46:10 -0500

    Fixed Assignment 3

--- AndieDewhurst/Assignment 3/Assignment3.m
+++ AndieDewhurst/Assignment 3/Assignment3.m
@@ -1,40 +1,40 @@
-%% 1
+%% Question 1
 A = ones(4, 5);
 A(3,4) = -1;
 A(:,1) = -1:-1:-4;
 
-%% 2
+%% Question 2
 B = zeros(size(A));
  B(3:4, 1:3) = A(2:3,2:4);
 
-%% 3
+%% Question 3 
 C = A+B;
 CLessZero = sum(C<0);
 C(C<0) = 0;
 
-%% 4
+%% Question 4
 D = mean(C);
 D_2 = sum(C)/size(C,2);
 
-%% 5
+%% Question 5
 E = sum(C,2)';
 
-%% 6
+%% Question 6
 F = zeros(size(E));
 
 for i = 1:length(E)
     F(i+1) = E(i)-D(i)';
 end
 
-%% 7
+%% Question 7
 G = rand(1,100);
-Gnorm = (G-min(G))/(max(G) - min(G));
+G = (G - (mean(G)) / std(G));
 
-%% 8 
+%% Question 8 
 H = reverse_rows(C,1);
 I = reverse_rows(C,2);
 
-%% 9
+%% Question 9
 x1 = 1:5;
 x2 = (1:3)';
 x3 = ones(3,3);
