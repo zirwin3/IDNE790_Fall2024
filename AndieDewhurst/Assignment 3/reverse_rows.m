42ccd73f75e21c7eb65963973296e77ef949490e
andiedewhurst <andie.dewhurst1208@gmail.com>
2024-09-19 12:06:11 -0500

    Update reverse_rows.m

--- AndieDewhurst/Assignment 3/reverse_rows.m
+++ AndieDewhurst/Assignment 3/reverse_rows.m
@@ -2,3 +2,8 @@ function y = reverse_rows(x,dim)
 %Technically correct is the best kind of correct?
 y = flip(x,dim);
 
+
+% test = rand(3,5);
+% test2 = test(end,:)
+% %grab each subsaquent row from test and vercat below test2
+% test2 = test2(2)
